05a6db799b389a3706391eaaf2c1a6a40451f8b2
Cielbird <guilhem.ane3@gmail.com>
2024-03-13 20:10:59 -0400

    added interface

--- controleur/controleur.ino
+++ controleur/controleur.ino
@@ -6,7 +6,7 @@
 const int PIN_BOUTON = A0;
 const int PIN_POSITION = A1;
 const int PIN_CURRENT = A2;
-const int PIN_PWM = 10;
+const int PIN_PWM = 3;
 
 
 //initialisation de l'écran LCD
@@ -19,73 +19,187 @@ const int PIN_PWM = 10;
 
 
 //Setup écran LCD
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);        // select the pins used on the LCD panel
 
-//Setup menu et bouton LCD
-int lcd_key = 0; 
-int adc_key_in = 0;
-unsigned long menuTriggeredTime = 0;
-int currentScreen = -1;
-const int numOfScreen = 2;
-String menu[numOfScreen][2] ={
-  {"Poid","Courrant = "},
-  {"Calibration", "Mettre poids : "}
+unsigned long tepTimer ;
+unsigned long buttonTimer;
+// 1:right, 2:up, 3:down, 4:left, 5:select, 0:nothing
+byte prevSelectedButton;
+// 0: pesée, 1: tarage, 2: étalonnage
+byte mode;
+
+unsigned int weights[] = {1, 2, 5, 10, 20, 50};
+byte selectedCalib;
+
+// characters speciaux
+byte fleches[8] = {
+  B00100,
+  B01010,
+  B10001,
+  B00000,
+  B10001,
+  B01010,
+  B00100,
 };
-int parameter[numOfScreen];
-bool updateScreen = true;
-bool boutonAlreadyPressed = false;//Pour empecher les boucles lorsqu'une fois pressé.
-
 
 //déclaration boucle de régulation (Présentement en position)
 ArduPID myController;
 double input;
 double output;
-double setpoint = 400;
+double current;
+double taredCurrent;
+double tare = 0.0;
+double setpoint = 16.4;
 
-
-//Les boutons vont varier la tension entrée A0. donc chaque bouton a un range spécifique à celui-ci.
-int read_LCD_buttons(){ // read the buttons
-  int adc_key_in = analogRead(PIN_BOUTON);
-  if(boutonAlreadyPressed | adc_key_in > 1000)
-  {return btnNONE;}
-  else
-  {
-    if (adc_key_in < 50) return btnRIGHT;
-    else if (adc_key_in < 250) return btnUP;
-    else if (adc_key_in < 450) return btnDOWN;
-    else if (adc_key_in < 650) return btnLEFT;
-    else if (adc_key_in < 850) return btnSELECT;
-    else return btnNONE;
+/**
+Returns a byte for the currently selected button. 1:right, 2:up, 3:down, 4:left, 5:select, 0:nothing
+**/
+byte getSelectedButton(){
+  int val = analogRead(0);                     // read the analog value for buttons
+  int selected;
+  if(val < 66){
+    selected = 1; // 0: right
+  }else if(val < 220){  
+    selected = 2; // 132: up
+  }else if(val < 395){
+    selected = 3; // 309: down
+  }else if(val < 602){
+    selected = 4; // 481: left
+  }else if(val < 963){  
+    selected = 5; // 722: select
+  }else{
+    selected = 0; // 1023: nothing
   }
- }
+  return selected;
+}
 
+void handleInput(byte button){
+  switch(button){
+    case 1: // right
+      mode = (mode+1)%3;
+      break;
+    case 4: // left
+      // can't use modulo 255 is not multiple of 3
+      if(mode==0)
+        mode = 2;
+      else
+        mode = mode-1;
+      break;
+    case 2: // up
+      selectedCalib = (selectedCalib+1)%6;
+      break;
+    case 3: // down
+      if(selectedCalib==0)
+        selectedCalib = 5;
+      else
+        selectedCalib = selectedCalib-1;
+      break;
+    case 5: // select
+      switch(mode){
+        case 1: //tarage
+          tareRoutine();
+          break;
+        case 2: //étalonnage
+          calibrate();
+      }
+  }
+}
 
-//Liste des menu???
-void navigationKey()
-{
-  if (lcd_key != btnNONE)
-  {
-    boutonAlreadyPressed = true;
-    switch (lcd_key)
-    {
-      case (btnRIGHT):
-        {}
+void calibrate(){
+  bool calibrating = true;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Calibration");
+  lcd.setCursor(0, 1);
+  lcd.print("en cours...");
+  delay(100);
+}
+
+void tareRoutine(){
+  bool taring = true;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Tare");
+  lcd.setCursor(0, 1);
+  lcd.print("en cours...");
+  tare = current;
+  delay(100);
+}
+
+void updateMenu(){
+  String lcdTopText;
+  if(millis() - tepTimer > 500){
+    tepTimer = millis();
+    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    switch(mode){
+      case 0: //pesée
+        lcdTopText = "< Peser >";
         break;
-      case (btnLEFT):
-        {}
+      case 1: //tarage
+        lcdTopText = "< Tarer >";
         break;
-      case (btnUP):
-        {}
+      case 2: //étalonnage
+        lcdTopText = "< Calibrer >";
+    }
+    lcd.print(lcdTopText);
+
+    switch(mode){
+      case 0: //pesée
+        lcd.setCursor(11, 1);
+        lcd.print(taredCurrent);
+        lcd.setCursor(15, 1);
+        lcd.print("g");
         break;
-      case (btnDOWN):
-        {}
+      case 1: //tarage
+        lcd.setCursor(0, 1);
+        lcd.print("OK");
+        lcd.setCursor(11, 1);
+        lcd.print(taredCurrent);
+        lcd.setCursor(15, 1);
+        lcd.print("g");
         break;
-      case (btnSELECT):
-        {}
-        break;
+      case 2: //étalonnage
+        lcd.setCursor(0, 1);
+        lcd.print("OK");
+        lcd.setCursor(12, 1);
+        lcd.print(weights[selectedCalib]);
+        lcd.print("g");
+        lcd.write(byte(0)); // fleches up down
     }
   }
+}
+
+void updateInput(){
+  if(millis() - buttonTimer > 50){
+    int s = getSelectedButton();
+    if(prevSelectedButton != s){
+      if(s != 0)
+        buttonTimer = millis();
+        handleInput(s);
+      prevSelectedButton = s;
+    }
+  }
+}
+
+double capteurInputToDist(int in)
+{
+  // convert arduino input to ampli output
+  double tension = in * (5.0 / 1023.0);
+  // convert ampli output to input
+  // obtenu en essai erreur
+  tension /= 9;    // K
+  tension += 0.94; // V offset
+  // convert capteur output to dist
+  // voir graphique 7.6
+  double C_1 = 5813.9;
+  double C_2 = 198.24;
+  double C_3 = 0.60750;
+  tension = max(tension, 0.9); // pour eviter des erreurs, min val
+  tension = min(tension, 2.69); // pour eviter des erreurs, max val
+  double a = pow(C_1 / (tension - C_3), 2.0/3) - C_2;
+  return sqrt(a);
 }
 
 void setup()
@@ -94,52 +208,39 @@ void setup()
 
   //Setup LCD screen
   lcd.begin(16, 2);
+  lcd.createChar(0, fleches);
 
   //SETUP régulateur de position.
-  double P = 0.1;
-  double I = 0.015;
+  double P = 0.001;
+  double I = 0.5;
   double D = 0.0;
   pinMode(PIN_POSITION, INPUT);
-  myController.begin(&input, &output, &setpoint, P, I, D, P_ON_E, BACKWARD, 20);
+  myController.begin(&input, &output, &setpoint, P, I, D, P_ON_E, FORWARD, 20);
   myController.setOutputLimits(-254, 254);
   myController.setWindUpLimits(-500, 500); // Groth bounds for the integral term to prevent integral wind-up
   myController.start();
 
   //Set PWM speed to 37KHz
   TCCR1B = TCCR1B & B11111000 | B00000010;
-  pinMode(10, OUTPUT);
+  pinMode(PIN_PWM, OUTPUT);
 }
 
-double capteurInputToDist(double tension)
-{
-  // convert arduino input to ampli output
-  tension = input * (5.0 / 1023.0)
-  // convert ampli output to input
-  // voir tableau 7.5 du rapport
-  tension /= 2.4; // K
-  tension += 0.4; // V offset
-  // convert capteur output to dist
-  // voir graphique 7.6
-  double C_1 = 5813.9;
-  double C_2 = 198.24;
-  double C_3 = 0.60750;
-  double a = pow(C_1 / (tension - C_3), 2.0/3) - C_2;
-  return sqrt(a);
-}
 
 void loop()
 {
   //Lecture du bouton analogique, peut-être changer pour une interruption en mode falling-edge avec les registres.
   //lcd_key = read_LCD_buttons(); // Read the current button state
   //navigationKey();
-  int in = analogRead(PIN_POSITION)
-  input = capteurInputToDist(in);
-  Serial.print("v: ");
-  Serial.print(voltage);
-  Serial.print("d pour 2: ");
-  Serial.print(capteurInputToDist(512));
+  int inPos = analogRead(PIN_POSITION);
+  double newInput = capteurInputToDist(inPos);
+  input = newInput;//(newInput + input)/2;
+  int inCur = analogRead(PIN_CURRENT);
+  current = inCur * (5.0 / 1023.0) * 60;
+  taredCurrent = current - tare;
+  Serial.println();
 
   myController.compute();
+  analogWrite(PIN_PWM, output+29); //Output for Vamp_in
   //Pour afficher les valeur de PID, et input, output, seulement enlever les commentaire.
   /*myController.debug(&Serial, "myController", PRINT_INPUT    | // Can include or comment out any of these terms to print
                                               PRINT_OUTPUT   | // in the Serial plotter
@@ -148,14 +249,11 @@ void loop()
   
   //myController.debug(&Serial, "myController",PRINT_BIAS);
 
-  //Cette section est utile pour trouver la position zero
-  //Serial.print("Valeur set = ");
-  //Serial.print(setpoint);
-  //Serial.print(" Valeur apercu = ");
-  //Serial.print(input);
-  //Serial.print(" Valeur Output = ");
-  //Serial.println(output);
-  Serial.print('\n');
-  analogWrite(10, output+29); //Output for Vamp_in
+  // input update
+  updateInput();
+
+  // print menu
+  updateMenu();
+
   delay(20);
 }
